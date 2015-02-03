a36b9a96edb772d4fee48f02e3ab522d95dfa488
Supriya Subudhi <supriya.subudhi@apigee.com>
2015-02-03 17:43:29 +0530

    overridden facts addition to in.py

--- in.py
+++ in.py
@@ -3,12 +3,12 @@ parser=argparse.ArgumentParser()
 parser.add_argument("-O", "--overridden_facts", dest="overridden_facts", default=False, help="any overridden facts")
 args = parser.parse_args()
 print 'hello' 
-#for ufkv in  args.overridden_facts.split(' '):
-#    key,value = ufkv.split('=')
-#    print 'arg[' + key + '] '+value
+for ufkv in  args.overridden_facts.split(' '):
+    key,value = ufkv.split('=')
+    print 'arg[' + key + '] '+value
 user_facts={}
-for user_input_fact in args.overridden_facts.split(' '):
-    key,value=user_input_fact.split('=')
-    user_facts[key]=value
+#for user_input_fact in args.overridden_facts.split(' '):
+ #   key,value=user_input_fact.split('=')
+  #  user_facts[key]=value
 
 
