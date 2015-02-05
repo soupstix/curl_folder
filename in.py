bdf7c4260bf58fed552db28cdf81977922e92828
Supriya Subudhi <supriya.subudhi@apigee.com>
2015-02-05 18:52:56 +0530

    trying with > instead of sys

--- in.py
+++ in.py
@@ -1,13 +1,13 @@
-import sys
 import argparse
+import sys
+import subprocess
 parser=argparse.ArgumentParser()
 parser.add_argument("-O", "--overridden_facts", dest="overridden_facts", default=False, help="any overridden facts")
 args = parser.parse_args()
 def msg(string):
     print string
     return 'returned string'
-stdout=sys.stdout
-sys.stdout=open('ofile','w')
+#sys.stdout=open('ofile','w')
 print 'hello' 
 print msg('hellooo')
 if args.overridden_facts :
